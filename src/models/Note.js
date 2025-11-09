bde9ac6e0c67832f4269ceb70dd4f7833645bac1
AndresTao <andrestao577@gmail.com>
2025-11-09 18:56:45 -0500

    continue

--- src/models/Note.js
+++ src/models/Note.js
@@ -1,46 +1,42 @@
+import { formatDateToBogota } from "../shared/utils/formatdate.js"
 
-
-import { formatDateToBogota } from "../shared/utils/formatdate"
 class Note {
-    constructor(title, description, user_id) {
-        this.id = Note.#generateId();
-        this.title = title;
-        this.description = description;
-        this.user_id = user_id;
-        this.creted_at = new Date();
-        this.updated_at = new Date();
-        this.fotmate_created_at = formatDateToBogota(this.creted_at);
-        this.formate_update_at = formatDateToBogota(this.updated_at);
-    };
+	constructor(title, description, user_id) {
+		this.id = Note.#generateId();
+		this.title = title;
+		this.description = description;
+		this.user_id = user_id;
+		this.creted_at = new Date();
+		this.updated_at = new Date();
+		this.fotmate_created_at = formatDateToBogota(this.creted_at);
+		this.formate_update_at = formatDateToBogota(this.updated_at);
+	};
 
+	updateTitle(newTitle) {
+		if (!(typeof title === "string")) {
+			throw new Error('Must be string')
+		}
+		if (!(title.length <= 100 && title.length >= 1)) {
+			throw new Error('Most be between 1 and 100 characters')
+		}
+		this.title = newTitle;
 
+	};
 
-    updateTitle(newTitle) {
-        this.title = newTitle;
+	updateDescription(description) {
 
-    };
+		if (!(typeof description === "string")) {
+			throw new Error('Must be string')
+		}
+		if (!(description.length <= 10000 && description.length >= 1)) {
+			throw new Error('Most be between 1 and 10000 characters')
+		}
+		this.description = description;
 
-    updateDescription(description) {
-        this.description = description;
+	};
 
-    };
-    static #generateId() {
-        const id = Math.floor(Math.random() * (110 - 2 + 1)) + 1;
-        return id;
-    }
+	static #generateId() {
+		const id = Math.floor(Math.random() * (110 - 2 + 1)) + 1;
+		return id;
+	}
 }
-
-
-
-const note1 = new Note(
-    "",
-    "",
-    1231
-);
-console.log(note1);
-
-note1.updateTitle("kkkk");
-console.log(note1);
-
-note1.updateDescription("day");
-console.log(note1);
